6a85960f0ff46447507f7391c5dc2a2c7a6b3d3d
Christian-Schultz <schultz.christian@gmail.com>
2017-09-12 22:18:21 +0200

    Added more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 import numpy as np
 import pandas as pd
+import corrmat
 
 @pytest.fixture(scope="function")
-def default_corrmat():
+def default_rand_mat():
     np.random.seed(0)
-    return pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=list('ABCD')).corr()
+    return pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=list('ABCD'))
+
+
+@pytest.fixture(scope="function")
+def default_corr_mat():
+    values = np.array([[1, -0.9, 0.5, 0.8], [-0.9, 1, 0.6, 0.4], [0.5, 0.6, 1, -0.4], [0.8, 0.4, -0.4, 1]])
+    df = pd.DataFrame(values, columns=list('ABCD'), index=list('ABCD'))
+    return corrmat.CorrelationMatrix(df)
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -2,14 +2,32 @@ import pytest
 import corrmat
 
 
-def test_non_symmetric_breaks(default_corrmat):
-    default_corrmat.iloc[0, 1] += 1
+def test_non_unity_diagonal_breaks(default_rand_mat):
+    df = default_rand_mat.corr()
+    df.iloc[0, 0] = 0.9
 
     with pytest.raises(ValueError):
-        corrmat.CorrelationMatrix(default_corrmat)
+        corrmat.CorrelationMatrix(df)
 
-def test_large_values_breaks(default_corrmat):
-    default_corrmat.iloc[0, 1] = 2
+
+def test_non_symmetric_breaks(default_rand_mat):
+    df = default_rand_mat.corr()
+    df.iloc[0, 1] += 1
 
     with pytest.raises(ValueError):
-        corrmat.CorrelationMatrix(default_corrmat)
+        corrmat.CorrelationMatrix(df)
+
+
+def test_large_values_breaks(default_rand_mat):
+    df = default_rand_mat.corr()
+    df.iloc[0, 1] = 2
+
+    with pytest.raises(ValueError):
+        corrmat.CorrelationMatrix(df)
+
+
+def test_get_submatrix(default_corr_mat):
+    cm = default_corr_mat
+
+    sm = cm.get_submatrix('A', threshold=0.8)
+    assert sm.matrix.equals(default_corr_mat.matrix[['A','D','B']].loc[['A','D','B']])
